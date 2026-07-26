da619e58eee04f13727cd49eb6fbf7d0f097f52e
tsungkailu <tsungkai.lu@gmail.com>
2026-07-27 07:04:06 +0800

    刪除照片: photos/戒茂斯登山口_1785086108405.webp

--- waypoint-data.js
+++ waypoint-data.js
@@ -155,11 +155,6 @@ var WAYPOINT_DATA = {
           "src": "photos/戒茂斯登山口_1785082117233.webp",
           "title": "Test",
           "body": "trst"
-        },
-        {
-          "src": "photos/戒茂斯登山口_1785086108405.webp",
-          "title": "新來橋",
-          "body": "新來橋"
         }
       ],
       "spur": false,
